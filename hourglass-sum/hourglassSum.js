1e4fa41532e423391744f78882df160e6a36ddfe
JasonEb <jasonebueng+git@gmail.com>
2020-06-15 19:06:23 -0700

    implements hourglass subset extractor

--- hourglass-sum/hourglassSum.js
+++ hourglass-sum/hourglassSum.js
@@ -1,10 +1,26 @@
 const hourglassSum = (arr) => {
-    debugger
     return arr
 }
 
 const hourglassSubset = (arr, i, j) => {
-    return arr;
+    // follows the form
+    /*
+        a b c
+          d
+        e f g
+    */
+
+    let a = arr[i][j]
+    let b = arr[i][j+1]
+    let c = arr[i][j+2]
+
+    let d = arr[i+1][j+1]
+
+    let e = arr[i+2][j]
+    let f = arr[i+2][j+1]
+    let g = arr[i+2][j+2]
+
+    return [a, b, c, d, e, f, g];
 }
 
 module.exports.hourglassSum = hourglassSum;
--- hourglass-sum/hourglassSum.test.js
+++ hourglass-sum/hourglassSum.test.js
@@ -1,5 +1,32 @@
 const {hourglassSum, hourglassSubset} = require('./hourglassSum');
 
+//hourglass subset test cases
+test('extracts hourglass subset at 0, 0', () => {
+    let arr = [
+        [-9,-9,-9,1,1,1],
+        [0,-9,0,4,3,2],
+        [-9,-9,-9,1,2,3],
+        [0,  0,  8,  6, 6, 0],
+        [0,  0,  0, -2, 0, 0],
+        [0,  0,  1,  2, 4, 0]
+    ]
+
+    expect(hourglassSubset(arr, 0, 0)).toEqual([-9, -9, -9, -9, -9, -9, -9])
+});
+
+test('extracts hourglass subset at 3, 3', () => {
+    let arr = [
+        [-9,-9,-9,1,1,1],
+        [0,-9,0,4,3,2],
+        [-9,-9,-9,1,2,3],
+        [0,  0,  8,  6, 6, 0],
+        [0,  0,  0, -2, 0, 0],
+        [0,  0,  1,  2, 4, 0]
+    ]
+
+    expect(hourglassSubset(arr, 3, 3)).toEqual([6, 6, 0, 0, 2, 4, 0])
+});
+
 // main case
 test('solves example case', () => {
     let arr = [
@@ -14,16 +41,3 @@ test('solves example case', () => {
     expect(hourglassSum(arr)).toBe(28)
 });
 
-//hourglass subset test cases
-test('extracts hourglass subset', () => {
-    let arr = [
-        [-9,-9,-9,1,1,1],
-        [0,-9,0,4,3,2],
-        [-9,-9,-9,1,2,3],
-        [0,  0,  8,  6, 6, 0],
-        [0,  0,  0, -2, 0, 0],
-        [0,  0,  1,  2, 4, 0]
-    ]
-
-    expect(hourglassSubset(arr, 0, 0)).toEqual([-9, -9, -9, -9, -9, -9, -9])
-});
