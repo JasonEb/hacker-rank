3e16b9921750763bb4f5e1beb71206f19fddc08c
JasonEb <jasonebueng@gmail.com>
2020-06-25 21:02:18 -0700

    sets up alternating characters
    initial implementations
    
    solves alternating characters + write up
    
    clears up formatting on readme

---
+++ alternating-characters/alternatingCharacters.js
@@ -0,0 +1,26 @@
+const alternatingCharacters = (str) => {
+    let count = 0
+    //iteratively go through str, examine if the next character needs to be 'deleted' 
+
+    while( str.length > 1) {
+        let i = 1
+        let c1 = str[0]
+        let c2 = str[i]
+
+        if (c1 == c2) {
+            while(c1 == c2) {
+                i++
+                count++
+                c2 = str[i]
+            }
+            
+        } 
+        str = str.substring(i, str.length)
+    }
+
+
+    return count
+}
+
+module.exports.alternatingCharacters = alternatingCharacters
+
